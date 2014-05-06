d2f5809f123586696dcf3683231471e84088a8cb
free2tedy <free2tedy@yahoo.com>
2014-05-07 01:06:42 +0300

    Update FileSystem

--- FileSharing/filesystem.cpp
+++ FileSharing/filesystem.cpp
@@ -1,9 +1,52 @@
 #include "filesystem.h"
 
-FileSystem::FileSystem(QObject *parent)
-	: QObject(parent)
-{
 
+FileSystem::FileSystem() 
+{
+	nrFiles = 0;
+}
+
+
+void FileSystem::addFile(const QString &filePath)
+{
+	QJsonObject newFile;
+	QFileInfo qfi(filePath);
+
+	newFile.insert(QString("index"), nrFiles);
+	newFile.insert(QString("size"), qfi.size());
+
+	filePaths.insert(nrFiles++, qfi.filePath());
+
+	fileList.insert(qfi.fileName(), newFile);
+}
+
+
+QString FileSystem::getFilePath(int index)
+{
+	return filePaths.value(index);
+}
+
+
+QJsonObject FileSystem::getFileList()
+{
+	return fileList;
+}
+
+
+QString FileSystem::fileListToString()
+{
+	return QJsonValue(fileList).toString();
+}
+
+
+QJsonObject FileSystem::fileListFromString(const QString &jsonString)
+{
+	return QJsonValue(jsonString).toObject();
+}
+
+QStringList FileSystem::getListOfFiles()
+{
+	return fileList.keys();
 }
 
 FileSystem::~FileSystem()
--- FileSharing/filesystem.h
+++ FileSharing/filesystem.h
@@ -2,16 +2,29 @@
 #define FILESYSTEM_H
 
 #include <QObject>
+#include <QJsonObject>
+#include <QStringList>
+#include <QMap>
+#include <QFileInfo>
 
 class FileSystem : public QObject
 {
 	Q_OBJECT
 
 public:
-	FileSystem(QObject *parent);
+	FileSystem();
+	QJsonObject getFileList();
+	QString getFilePath(int index);
+	QString fileListToString();
+	QJsonObject fileListFromString(const QString &jsonString);
+	QStringList getListOfFiles();
+	void addFile(const QString &fileName);
 	~FileSystem();
 
 private:
+	QJsonObject fileList;
+	QMap<int, QString> filePaths;
+	int nrFiles;
 	
 };
 
